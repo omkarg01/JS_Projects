b4db7ccf02012ec7deb38925d592c8e467feb12d
omkarg01 <omkargujja01@gmail.com>
2022-10-13 22:54:13 +0530

    get all elements

---
+++ COUNTDOWN APP/script.js
@@ -0,0 +1,4 @@
+const text = document.querySelector("input");
+const start = document.querySelector("#start");
+const reset = document.querySelector("#reset");
+const display = document.querySelector("#display");
