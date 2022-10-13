09d3434145526b7af1f082706466017e7d224b49
omkarg01 <omkargujja01@gmail.com>
2022-10-13 21:23:27 +0530

    update history done

--- EXPENSE TRACKER APP/script.js
+++ EXPENSE TRACKER APP/script.js
@@ -1,9 +1,12 @@
 const addBtn = document.querySelector("#btn");
 const list = document.querySelector("#list");
 const balance = document.querySelector("#balance");
+let deleteBtn = document.querySelectorAll(".delete-btn");
+let count = 3;
 
 addBtn.addEventListener("click", addTransaction);
 function addTransaction(event) {
+    count += 1;
     event.preventDefault()
     const text = document.querySelector("#text");
     const amount = document.querySelector("#amount");
@@ -13,12 +16,35 @@ function addTransaction(event) {
     transaction.innerHTML = `
     <span>${text.value}</span>
     <span>${amount.value}</span>
-    <button class="delete-btn">x</button>
+    <button id=${count} class="delete-btn">x</button>
     `;
     list.appendChild(transaction);
     balance.innerText = parseInt(balance.innerText) + parseInt(amount.value) + ".00";
     text.value = "";
     amount.value = "";
+    deleteBtn = document.querySelectorAll(".delete-btn");
+    transaction.addEventListener("click", deleteTransaction);
 }
 
+deleteBtn.forEach(btn => {
+    btn.addEventListener("click", deleteTransaction);
+})
 
+function deleteTransaction(event) {
+    const transaction = event.target.parentElement;
+    const amount = parseInt(transaction.children[1].innerText);
+    balance.innerText = parseInt(balance.innerText) - amount + ".00";
+    transaction.remove();
+    updateHistory();
+}
+
+function updateHistory() {
+    const history = document.querySelectorAll("li");
+    const list = document.querySelector("#list");
+    // console.log(history.length);
+    if (history.length === 0) {
+        balance.innerText = 0;
+        list.innerHTML = "No transactions yet";
+    }
+
+}
