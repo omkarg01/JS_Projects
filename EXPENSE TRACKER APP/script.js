3e4f8f16eed59a3a277585585d48636742d8ed2c
omkarg01 <omkargujja01@gmail.com>
2022-10-13 20:09:12 +0530

    add transaction done

--- EXPENSE TRACKER APP/script.js
+++ EXPENSE TRACKER APP/script.js
@@ -1,6 +1,21 @@
-const text = document.querySelector("#text");
-const amount = document.querySelector("#amount");
-const addBtn = document.querySelector("#add");
+const addBtn = document.querySelector("#btn");
 const list = document.querySelector("#list");
-const balance = document.querySelector("#balance"); 
+const balance = document.querySelector("#balance");
 
+addBtn.addEventListener("click", addTransaction);
+function addTransaction(event) {
+    event.preventDefault()
+    const text = document.querySelector("#text");
+    const amount = document.querySelector("#amount");
+    const type = amount.value > 0 ? "income" : "expense";
+    const transaction = document.createElement("li");
+    transaction.classList.add(type);
+    transaction.innerHTML = `
+    <span>${text.value}</span>
+    <span>${amount.value}</span>
+    <button class="delete-btn">x</button>
+    `;
+    text.value = "";
+    amount.value = "";
+    list.appendChild(transaction);
+}
