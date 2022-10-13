a63a7aa1a82ffd6926addd3e06495627e35db18f
omkarg01 <omkargujja01@gmail.com>
2022-10-13 19:44:08 +0530

    get all elements

---
+++ EXPENSE TRACKER APP/script.js
@@ -0,0 +1,6 @@
+const text = document.querySelector("#text");
+const amount = document.querySelector("#amount");
+const addBtn = document.querySelector("#add");
+const list = document.querySelector("#list");
+const balance = document.querySelector("#balance"); 
+
