a4bc36710ed17a697a6ae509b407e6eafce21ecd
omkarg01 <omkargujja01@gmail.com>
2022-10-13 23:03:05 +0530

    countdown app done

--- COUNTDOWN APP/script.js
+++ COUNTDOWN APP/script.js
@@ -2,3 +2,18 @@ const text = document.querySelector("input");
 const start = document.querySelector("#start");
 const reset = document.querySelector("#reset");
 const display = document.querySelector("#display");
+
+start.addEventListener("click", (event) => {
+    event.preventDefault();
+    let count = text.value;
+    if (count > 0) {
+        const countdown = setInterval(() => {
+            display.innerHTML = count;
+            count--;
+            if (count < 0) {
+                clearInterval(countdown);
+                display.innerHTML = "Time is up!";
+            }
+        }, 1000);
+    }
+})
