d1ae88f4c4062830a825ddd71529fc2879ce72a2
omkarg01 <omkargujja01@gmail.com>
2022-10-13 22:43:14 +0530

    Palindrome Checker

--- PALINDROME CHECKER APP/script.js
+++ PALINDROME CHECKER APP/script.js
@@ -4,9 +4,12 @@ const output = document.querySelector("#output");
 
 button.addEventListener("click", (event) => {
     event.preventDefault()
-    const inputText = input.value;
+    let inputText = input.value;
+    inputText = inputText.toLowerCase();
     const reversedText = inputText.split("").reverse().join("");
     output.style.display = "block";
+
+    console.log(inputText);
 
     if (inputText.length === 0) {
         output.innerText = "Please enter a word or phrase";
